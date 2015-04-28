67abea078d592c76b37f9ad1afbce2157f4f421f
hellpanderrr <hellpanderrr@gmail.com>
2015-04-28 07:40:38 +0300

    Start
    1

---
+++ define.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module defines the global constants.
+"""
+
+import os
+import getpass
+import tempfile
+
+AUTH_URL = 'https://accounts.coursera.org/api/v1/login'
+AUTH_URL_V3 = 'https://www.coursera.org/api/login/v3'
+CLASS_URL = 'https://class.coursera.org/{class_name}'
+OPENCOURSE_CONTENT_URL = 'https://www.coursera.org/api/opencourse.v1/course/{class_name}'
+OPENCOURSE_VIDEO_URL = 'https://www.coursera.org/api/opencourse.v1/video/{video_id}'
+ABOUT_URL = 'https://api.coursera.org/api/catalog.v1/courses?' \
+            'fields=largeIcon,photo,previewLink,shortDescription,smallIcon,' \
+            'smallIconHover,universityLogo,universityLogoSt,video,videoId,' \
+            'aboutTheCourse,targetAudience,faq,courseSyllabus,courseFormat,' \
+            'suggestedReadings,instructor,estimatedClassWorkload,' \
+            'aboutTheInstructor,recommendedBackground&' \
+            'q=search&query={class_name}'
+AUTH_REDIRECT_URL = 'https://class.coursera.org/{class_name}' \
+                    '/auth/auth_redirector?type=login&subtype=normal'
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# define a per-user cache folder
+if os.name == "posix":  # pragma: no cover
+    import pwd
+    user = pwd.getpwuid(os.getuid())[0]
+else:
+    user = getpass.getuser()
+
+PATH_CACHE = os.path.join(tempfile.gettempdir(), user + "_coursera_dl_cache")
+PATH_COOKIES = os.path.join(PATH_CACHE, 'cookies')
---
+++ utils.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module provides utility functions that are used within the script.
+"""
+
+import errno
+import os
+import random
+import re
+import string
+import sys
+
+import six
+
+from six.moves import html_parser
+
+#  six.moves doesn’t support urlparse
+if six.PY3:  # pragma: no cover
+    from urllib.parse import urlparse
+else:
+    from urlparse import urlparse
+
+# Python3 (and six) don't provide string
+if six.PY3:
+    from string import ascii_letters as string_ascii_letters
+    from string import digits as string_digits
+else:
+    from string import letters as string_ascii_letters
+    from string import digits as string_digits
+
+
+if six.PY2:
+    def decode_input(x):
+        stdin_encoding = sys.stdin.encoding
+        if stdin_encoding is None:
+            stdin_encoding = "UTF-8"
+        return x.decode(stdin_encoding)
+else:
+    def decode_input(x):
+        return x
+
+
+def random_string(length):
+    """
+    Return a pseudo-random string of specified length.
+    """
+    valid_chars = string_ascii_letters + string_digits
+
+    return ''.join(random.choice(valid_chars) for i in range(length))
+
+
+def clean_filename(s, minimal_change=False):
+    """
+    Sanitize a string to be used as a filename.
+
+    If minimal_change is set to true, then we only strip the bare minimum of
+    characters that are problematic for filesystems (namely, ':', '/' and
+    '\x00', '\n').
+    """
+
+    # First, deal with URL encoded strings
+    h = html_parser.HTMLParser()
+    s = h.unescape(s)
+
+    # strip paren portions which contain trailing time length (...)
+    s = (
+        s.replace(':', '-')
+        .replace('/', '-')
+        .replace('\x00', '-')
+        .replace('\n', '')
+    )
+
+    if minimal_change:
+        return s
+
+    s = s.replace('(', '').replace(')', '')
+    s = s.rstrip('.')  # Remove excess of trailing dots
+
+    s = s.strip().replace(' ', '_')
+    valid_chars = '-_.()%s%s' % (string.ascii_letters, string.digits)
+    return ''.join(c for c in s if c in valid_chars)
+
+
+def get_anchor_format(a):
+    """
+    Extract the resource file-type format from the anchor.
+    """
+
+    # (. or format=) then (file_extension) then (? or $)
+    # e.g. "...format=txt" or "...download.mp4?..."
+    fmt = re.search(r"(?:\.|format=)(\w+)(?:\?.*)?$", a)
+    return fmt.group(1) if fmt else None
+
+
+def mkdir_p(path, mode=0o777):
+    """
+    Create subdirectory hierarchy given in the paths argument.
+    """
+
+    try:
+        os.makedirs(path, mode)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def fix_url(url):
+    """
+    Strip whitespace characters from the beginning and the end of the url
+    and add a default scheme.
+    """
+    if url is None:
+        return None
+
+    url = url.strip()
+
+    if url and not urlparse(url).scheme:
+        url = "http://" + url
+
+    return url
